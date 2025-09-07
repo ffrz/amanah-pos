ef9e535de8d9bd75badbc536ddceac7a970e4351
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2025-09-07 07:00:54 +0700

    added checkbox component

---
+++ resources/js/components/CheckBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <div style="margin-left: -10px">
+    <q-checkbox
+      class="full-width q-pl-none"
+      :model-value="modelValue"
+      @update:model-value="(value) => $emit('update:modelValue', value)"
+      :disable="disable"
+      :label="label"
+    />
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+  modelValue: {
+    type: [Boolean, Array],
+    default: false,
+  },
+  label: {
+    type: String,
+    default: "",
+  },
+  disable: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+defineEmits(["update:modelValue"]);
+</script>
