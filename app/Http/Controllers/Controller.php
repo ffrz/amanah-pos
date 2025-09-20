7a133ac45e5357cba90d7d6441b159980d4889a1
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2025-09-20 07:52:11 +0700

    authorization

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -16,7 +16,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
 abstract class Controller
 {
+    use AuthorizesRequests, ValidatesRequests;
+
     //
 }
