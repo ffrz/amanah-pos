0140f442e1761dd18b2e481c7a0a57c4e0c2fca1
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2025-10-24 09:51:25 +0700

    added link target

--- resources/js/components/MyLink.vue
+++ resources/js/components/MyLink.vue
@@ -1,25 +1,35 @@
 <script setup>
-import { router } from '@inertiajs/vue3';
+import { router } from "@inertiajs/vue3";
 
 const props = defineProps({
   href: {
     type: String,
-    default: '',
+    default: "",
   },
   method: {
     type: String,
-    default: 'get',
+    default: "get",
   },
   label: {
     type: String,
-    default: '',
-  }
+    default: "",
+  },
+  target: {
+    type: String,
+    default: "_self",
+  },
 });
 
+const openLink = () => {
+  if (props.target == "_self") {
+    router[props.method](props.href);
+  }
+  window.open(props.href, props.target);
+};
 </script>
 
 <template>
-  <a @click.prevent="router[props.method](props.href)" :href="props.href">
+  <a @click.prevent="openLink" :href="props.href" :target="props.target">
     <template v-if="props.label">
       {{ props.label }}
     </template>
