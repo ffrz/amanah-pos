1ff126558b632674e4af5a83b476df5e22ed5295
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2025-11-21 10:17:18 +0700

    supplier wallet

---
+++ app/Models/SupplierWalletTransaction.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ * Proprietary Software / Perangkat Lunak Proprietary
+ * Copyright (c) 2025 Fahmi Fauzi Rahman. All rights reserved.
+ *
+ * EN: Unauthorized use, copying, modification, or distribution is prohibited.
+ * ID: Penggunaan, penyalinan, modifikasi, atau distribusi tanpa izin dilarang.
+ *
+ * See the LICENSE file in the project root for full license information.
+ * Lihat file LICENSE di root proyek untuk informasi lisensi lengkap.
+ *
+ * GitHub: https://github.com/ffrz
+ * Email: fahmifauzirahman@gmail.com
+ */
+
+namespace App\Models;
+
+use App\Models\Traits\HasDocumentVersions;
+use App\Models\Traits\HasTransactionCode;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SupplierWalletTransaction extends BaseModel
+{
+    use HasFactory,
+        HasDocumentVersions,
+        HasTransactionCode,
+        SoftDeletes;
+
+    protected string $transactionPrefix = 'SWTX';
+
+    protected $fillable = [
+        'supplier_id',
+        'finance_account_id',
+        'code',
+        'datetime',
+        'type',
+        'amount',
+        'image_path',
+        'ref_type',
+        'ref_id',
+        'notes',
+    ];
+
+    protected $appends = [
+        'type_label',
+
+    ];
+
+    /**
+     * Transaction types.
+     */
+    const Type_Adjustment = 'adjustment';
+    const Type_Refund = 'refund';
+    const Type_Deposit = 'deposit';
+    const Type_PurchaseOrderPayment = 'purchase_order_payment';
+    const Type_PurchaseOrderRefund = 'purchase_order_refund';
+    const Type_Withdrawal = 'withdrawal';
+
+    const Types = [
+        self::Type_Adjustment => 'Penyesuaian',
+        self::Type_Deposit => 'Depoist',
+        self::Type_Refund => 'Refund',
+        self::Type_Withdrawal => 'Penarikan',
+        self::Type_PurchaseOrderPayment => 'Pembelian',
+        self::Type_PurchaseOrderRefund => 'Refund Pembelian',
+    ];
+
+    const RefType_PurchaseOrderPayment = 'purchase_order_payment';
+    const RefType_PurchaseOrderReturnRefund = 'purchase_order_return_refund';
+    const RefType_SupplierWalletTransactionConfirmation = 'supplier_wallet_transaction_confirmation';
+
+    const RefTypes = [
+        self::RefType_PurchaseOrderPayment => 'Transaksi Pembayaran Penjualan',
+        self::RefType_PurchaseOrderReturnRefund => ' Transaksi Refund Pembayaran Penjualan',
+        self::RefType_SupplierWalletTransactionConfirmation => 'Konfirmasi Transaksi Wallet Pelanggan',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'supplier_id' => 'integer',
+            'finance_account_id' => 'integer',
+            'datetime' => 'datetime',
+            'type' => 'string',
+            'amount' => 'decimal:2',
+            'ref_type' => 'string',
+            'ref_id' => 'integer',
+            'notes' => 'string',
+            'created_by' => 'integer',
+            'updated_by' => 'integer',
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+            'image_path' => 'string'
+        ];
+    }
+
+    public function getTypeLabelAttribute()
+    {
+        return self::Types[$this->type] ?? '-';
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class, 'supplier_id');
+    }
+
+    public function financeAccount()
+    {
+        return $this->belongsTo(FinanceAccount::class, 'finance_account_id');
+    }
+}
---
+++ database/migrations/2025_11_21_092629_create_supplier_wallet_transactions_table.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Proprietary Software / Perangkat Lunak Proprietary
+ * Copyright (c) 2025 Fahmi Fauzi Rahman. All rights reserved.
+ * 
+ * EN: Unauthorized use, copying, modification, or distribution is prohibited.
+ * ID: Penggunaan, penyalinan, modifikasi, atau distribusi tanpa izin dilarang.
+ * 
+ * See the LICENSE file in the project root for full license information.
+ * Lihat file LICENSE di root proyek untuk informasi lisensi lengkap.
+ * 
+ * GitHub: https://github.com/ffrz
+ * Email: fahmifauzirahman@gmail.com
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('supplier_wallet_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('finance_account_id')->nullable()->constrained('finance_accounts')->onDelete('restrict');
+            $table->foreignId('supplier_id')->nullable()->constrained('suppliers')->onDelete('restrict');
+
+            $table->string('code', 255)->unique();
+            $table->nullableMorphs('ref');
+
+            $table->datetime('datetime')->nullable()->index();
+            $table->string('type', 30)->index();
+            $table->decimal('amount', 12, 2)->default(0.);
+
+            $table->string('image_path', 255)->nullable()->default('');
+            $table->text('notes')->nullable();
+
+            $table->createdUpdatedDeletedTimestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('supplier_wallet_transactions');
+    }
+};
