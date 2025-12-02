79444861c69c26d5b930c1aa9de3da5a0f061c28
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2025-12-02 17:38:04 +0700

    Update 2025_12_02_165331_change_document_date_to_datetime_in_stock_movements.php

--- database/migrations/2025_12_02_165331_change_document_date_to_datetime_in_stock_movements.php
+++ database/migrations/2025_12_02_165331_change_document_date_to_datetime_in_stock_movements.php
@@ -10,36 +10,28 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('stock_movements', function (Blueprint $table) {
-            if (!Schema::hasColumn('stock_movements', 'party_code')) {
-                $table->string('party_code', 50)
-                    ->nullable()
-                    ->after('party_type')
-                    ->index();
-            }
+            $table->string('party_code', 50)
+                ->nullable()
+                ->after('party_type')
+                ->index();
 
-            if (!Schema::hasColumn('stock_movements', 'document_datetime')) {
-                $table->dateTime('document_datetime')
-                    ->nullable()
-                    ->after('document_code');
-            }
-
-            if (Schema::hasColumn('stock_movements', 'document_date')) {
-                DB::statement("
-                    UPDATE stock_movements
-                    SET document_datetime =
-                        CASE
-                            WHEN document_date IS NOT NULL
-                            THEN CONCAT(document_date, ' 00:00:00')
-                            ELSE NULL
-                        END
-                ");
-            }
+            $table->dateTime('document_datetime')
+                ->nullable()
+                ->after('document_code');
         });
 
+        DB::statement("
+            UPDATE stock_movements
+            SET document_datetime =
+                CASE
+                    WHEN document_date IS NOT NULL
+                    THEN CONCAT(document_date, ' 00:00:00')
+                    ELSE NULL
+                END
+        ");
+
         Schema::table('stock_movements', function (Blueprint $table) {
-            if (Schema::hasColumn('stock_movements', 'document_date')) {
-                $table->dropColumn('document_date');
-            }
+            $table->dropColumn('document_date');
         });
     }
 
@@ -50,14 +42,15 @@ return new class extends Migration
 
             // Kembalikan kolom lama
             $table->date('document_date')->nullable()->after('document_code');
+        });
 
-            // Copy back jika ingin
-            DB::statement("
+        DB::statement("
                 UPDATE stock_movements
                 SET document_date = DATE(document_datetime)
                 WHERE document_datetime IS NOT NULL
             ");
 
+        Schema::table('stock_movements', function (Blueprint $table) {
             $table->dropColumn('document_datetime');
         });
     }
