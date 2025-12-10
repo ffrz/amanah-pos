88776d75a7eb5d09fde19227d470e2bbd9cdede3
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2025-12-11 05:35:49 +0700

    disable button for non closed status

--- resources/js/modules/admin/pages/sales-order/detail/PaymentTab.vue
+++ resources/js/modules/admin/pages/sales-order/detail/PaymentTab.vue
@@ -4,7 +4,9 @@ import { computed, ref } from "vue";
 import PaymentDialog from "./PaymentDialog.vue";
 import { useQuasar } from "quasar";
 import axios from "axios";
-import { router } from "@inertiajs/vue3";
+import { router, usePage } from "@inertiajs/vue3";
+
+const page = usePage();
 
 const props = defineProps({
   data: Object,
@@ -124,6 +126,7 @@ const deletePayment = (payment) => {
           dense
           class="custom-dense"
           size="sm"
+          :disable="page.props.data.status != 'closed'"
           @click.stop="openPaymentDialog()"
         />
       </div>
